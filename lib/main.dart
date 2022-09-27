1087d718a04d0701dade362135444521e2b224ce
FlutLab.io <>
2022-09-27 08:06:11 +0000

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:json_theme/json_theme.dart';
+import 'package:flutter/services.dart'; // For rootBundle
+import 'dart:convert'; // For jsonDecode
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final themeStr = await rootBundle.loadString('assets/appainter_theme.json');
+  final themeJson = jsonDecode(themeStr);
+  final theme = ThemeDecoder.decodeThemeData(themeJson)!;
+  runApp(ToggleButtonRun(theme: theme));
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class ToggleButtonRun extends StatefulWidget {
+  final ThemeData theme;
+  const ToggleButtonRun({Key? key, required this.theme}) : super(key: key);
+
+  @override
+  _ToggleButtonRunState createState() => _ToggleButtonRunState();
+}
+
+class _ToggleButtonRunState extends State<ToggleButtonRun> {
+  final List<bool> _selections = List.generate(3, (_) => false);
+
+  FontWeight txtBold = FontWeight.normal;
+  FontStyle txtItalic = FontStyle.normal;
+  TextDecoration txtUnder = TextDecoration.none;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      debugShowCheckedModeBanner: false,
+      theme: widget.theme,
+      title: 'PoC Toggle Buttons',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Toggle Buttons PoC Demo"),
+            centerTitle: true,
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Container(
+                  margin: const EdgeInsets.all(20),
+                  child: ToggleButtons(
+                    children: const <Widget>[
+                      Icon(Icons.format_bold, size: 100),
+                      Icon(Icons.format_italic, size: 100),
+                      Icon(Icons.format_underlined, size: 100),
+                    ],
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+                    isSelected: _selections,
+                    onPressed: (int index) {
+                      setState(() {
+                        _selections[index] = !_selections[index];
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+                        if (index == 0 && _selections[index]) {
+                          txtBold = FontWeight.bold;
+                        } else if (index == 0 && !_selections[index]) {
+                          txtBold = FontWeight.normal;
+                        }
+
+                        if (index == 1 && _selections[index]) {
+                          txtItalic = FontStyle.italic;
+                        } else if (index == 1 && !_selections[index]) {
+                          txtItalic = FontStyle.normal;
+                        }
+
+                        if (index == 2 && _selections[index]) {
+                          txtUnder = TextDecoration.underline;
+                        } else if (index == 2 && !_selections[index]) {
+                          txtUnder = TextDecoration.none;
+                        }
+                      });
+                    },
+                    // color: Colors.teal,
+                    // fillColor: Colors.deepPurple,
+                  )),
+              Container(
+                  margin: EdgeInsets.all(20),
+                  child: Text(
+                    "This is a demonstration and proof of concept for using toggle buttons. click/tap a button to activate it. do the same to deactivate it.",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: txtBold,
+                      fontStyle: txtItalic,
+                      decoration: txtUnder,
+                    ),
+                    textAlign: TextAlign.center,
+                  ))
+            ],
+          )),
     );
   }
 }
